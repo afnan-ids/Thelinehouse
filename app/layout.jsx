ad8d284df4c8710fe5a7ba3eccc7eb5c230aef53
afnan <afnanalzaidi03@gmail.com>
2026-02-07 21:36:07 -0800

    first commit c-1

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,11 +3,6 @@ import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
-// export const metadata = {
-//   title: 'The Linen House - Manufacturer & Supplier of Fabrics & Uniforms',
-//   description: 'The Linen House is a distinguished manufacturer & supplier of a broad range of fabrics, hospital linen, corporate uniforms, industrial wears, and more with extensive customization options.',
-// };
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
